14652a71507e0ab8f087b35ef0ab669d2d349e0f
Sarah Elzinga <sevans924@gmail.com>
2019-03-24 02:36:57 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) # put argument(s) here
+if block_given?
+  i = 0
+  while i < array.length
+      yield array[i]
+      i += 1
+    end
+  else
+    puts "No block was given!"
+  end
+    # code here
 end
